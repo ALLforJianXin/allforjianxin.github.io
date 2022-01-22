faafad24af679241e7ff870f24ddcf4dccdd8800
chobitly <chobitly@gmail.com>
2022-01-22 20:21:27 +0800

    调整右上角目录

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -53,12 +53,8 @@ module.exports = [
     link: '/research/',
     items: [
       { text: '糖渣合集', link: '/research/#糖渣合集' },
-      {
-        text: '嗑学分析',
-        link: '/research/#嗑学分析', items: [
-          { text: '非主流戬心人', link: '/pages/a85947/' },
-        ],
-      },
+      { text: '嗑学分析', link: '/research/#嗑学分析' },
+      { text: '非主流戬心人', link: '/pages/a85947/' },
       { text: '扩展考据', link: '/research/#扩展考据' },
     ],
   },
