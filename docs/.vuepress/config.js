73e58c63e67e1b46afa463be001ca73bbb0f76eb
chobitly <chobitly@gmail.com>
2022-02-19 17:22:27 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  title: "戬心Press",
-  description: '戬心同人站，致敬每个爱他们的你。',
+  title: "戬心同人站",
+  description: '戬心Press同人站，致敬每个爱他们的你。',
   // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
